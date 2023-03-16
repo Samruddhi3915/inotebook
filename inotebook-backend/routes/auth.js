d0c4240854fee1d31c5488fa79fcc57c40124001
Samruddhi3915 <samruddhi3915@gmail.com>
2023-03-17 00:24:29 +0530

    Validations for collections

--- inotebook-backend/routes/auth.js
+++ inotebook-backend/routes/auth.js
@@ -18,26 +18,31 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
     // check whether the user with same email exists already
-    let user = await User.findOne({ email: req.body.email });
-    // console.log(user);
-    if (user) {
-      return res
-        .status(400)
-        .json({ error: "Sorry a user with this email already exists!" });
-    }
-    //creating user
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      // console.log(user);
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists!" });
+      }
+      //creating user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
 
-    //   .then((user) => res.json(user))
-    //   .catch((err) => {
-    //     console.log(err);
-    //     res.json({ error: "Please enter a unique value for email" });
-    //   });
-    res.send("response sent");
+      //   .then((user) => res.json(user))
+      //   .catch((err) => {
+      //     console.log(err);
+      //     res.json({ error: "Please enter a unique value for email" });
+      //   });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error oocured");
+    }
   }
 );
 module.exports = router;
